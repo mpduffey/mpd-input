6e9fb2788076da031582f277af688d1d8774cbb5
Mike Duffey <mpduffey@gmail.com>
2015-12-12 17:30:03 +0000

    Sat Dec 12 17:28:29 UTC 2015

--- mpd-input.js
+++ mpd-input.js
@@ -3,4 +3,19 @@ angular.module('mpd.input', [])
 	return {
 		
 	};
+})
+.directive('focusMe', function($timeout) {
+return {
+	link: function(scope, element, attrs) {
+		scope.$watch(attrs.focusMe, function(value) {
+			if(value === true) { 
+				console.log('value=',value);
+				$timeout(function() {
+					element[0].focus();
+					scope[attrs.focusMe] = false;
+				});
+			}
+		});
+	}
+};
 });
